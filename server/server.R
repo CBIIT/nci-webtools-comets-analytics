035f5e15214db547b8b2504025f0f8682a24f1ca
Brian Park <park-brian@users.noreply.github.com>
2021-08-16 11:16:53 -0400

    do not serve static files from backend

--- server/server.R
+++ server/server.R
@@ -8,5 +8,4 @@ pr() |>
   pr_mount("/api", pr("comets.R")) |>
   pr_set_docs(FALSE) |>
   pr_set_debug(TRUE) |>
-  pr_static(path = "/", direc = config$server$static) |>
   pr_run(host = "0.0.0.0", port = config$server$port)
