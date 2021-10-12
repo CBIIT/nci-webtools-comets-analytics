783d9f7efd1f4afe1651eaac264a39abfd2dc243
Brian Park <park-brian@users.noreply.github.com>
2021-10-12 12:58:24 -0400

    remove unused imports

--- client/src/modules/analysis/results/heatmap-results.js
+++ client/src/modules/analysis/results/heatmap-results.js
@@ -5,12 +5,18 @@ import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
 import Plot from "../../common/plot";
 import { useRecoilState } from "recoil";
-import { groupBy, pick, cloneDeep, chunk, uniq, map } from "lodash";
+import { cloneDeep, uniq, map } from "lodash";
 import { heatmapOptionsState } from "../analysis.state";
-import { getHeatmapPlot, getHeatmapDendrogramPlot } from "./heatmap-results.utils";
+import {
+  getHeatmapPlot,
+  getHeatmapDendrogramPlot,
+} from "./heatmap-results.utils";
 export default function HeatmapResults({ results }) {
-  const [heatmapOptions, setHeatmapOptions] = useRecoilState(heatmapOptionsState);
-  const mergeHeatmapOptions = (value) => setHeatmapOptions({ ...heatmapOptions, ...value });
+  const [heatmapOptions, setHeatmapOptions] = useRecoilState(
+    heatmapOptionsState
+  );
+  const mergeHeatmapOptions = (value) =>
+    setHeatmapOptions({ ...heatmapOptions, ...value });
 
   const { xKey } = heatmapOptions;
   const records = cloneDeep(results?.Effects) || [];
@@ -18,13 +24,17 @@ export default function HeatmapResults({ results }) {
   const xCategoriesSorted = cloneDeep(xCategories).sort();
 
   const heatmapPlot = getHeatmapPlot(results, heatmapOptions);
-  const heatmapDendrogramPlot = getHeatmapDendrogramPlot(results, heatmapOptions);
-  const selectedHeatmapPlot = heatmapOptions.showDendrogram ? heatmapDendrogramPlot : heatmapPlot;
+  const heatmapDendrogramPlot = getHeatmapDendrogramPlot(
+    results,
+    heatmapOptions
+  );
+  const selectedHeatmapPlot = heatmapOptions.showDendrogram
+    ? heatmapDendrogramPlot
+    : heatmapPlot;
 
   function handleChange(event) {
     let { name, value, type, checked } = event.target;
-    if (type === 'checkbox')
-      value = checked;
+    if (type === "checkbox") value = checked;
     mergeHeatmapOptions({ [name]: value });
   }
 
@@ -35,7 +45,11 @@ export default function HeatmapResults({ results }) {
           <Col md={4}>
             <Form.Group className="mb-3" controlId="sortRow">
               <Form.Label>Sort Strata By</Form.Label>
-              <Form.Select name="sortRow" value={heatmapOptions.sortRow} onChange={handleChange} disabled={heatmapOptions.showDendrogram}>
+              <Form.Select
+                name="sortRow"
+                value={heatmapOptions.sortRow}
+                onChange={handleChange}
+                disabled={heatmapOptions.showDendrogram}>
                 <option>All participants (no stratification)</option>
               </Form.Select>
             </Form.Group>
@@ -43,12 +57,19 @@ export default function HeatmapResults({ results }) {
           <Col md={4}>
             <Form.Group className="mb-3" controlId="sortColumn">
               <Form.Label>Sort Outcomes By</Form.Label>
-              <Form.Select name="sortColumn" value={heatmapOptions.sortColumn} onChange={handleChange} disabled={heatmapOptions.showDendrogram}>
-                {xCategoriesSorted.map(label => <option value={label} key={label}>{label}</option>)}
+              <Form.Select
+                name="sortColumn"
+                value={heatmapOptions.sortColumn}
+                onChange={handleChange}
+                disabled={heatmapOptions.showDendrogram}>
+                {xCategoriesSorted.map((label) => (
+                  <option value={label} key={label}>
+                    {label}
+                  </option>
+                ))}
               </Form.Select>
             </Form.Group>
           </Col>
-
         </Row>
 
         <Form.Group className="mb-1" controlId="showAnnotations">
@@ -68,17 +89,20 @@ export default function HeatmapResults({ results }) {
             checked={heatmapOptions.showDendrogram}
             onChange={handleChange}
             disabled={!results.heatmap.dendrogram}
-            label={<>
-              Show Hierarchical Clustering
-              <OverlayTrigger
-                overlay={
-                  <Tooltip id="showMetabolitesTooltip">
-                    Requires at least 2 exposures and outcomes
-                  </Tooltip>
-                }>
-                <i className="bi bi-info-circle ms-1" />
-              </OverlayTrigger>
-            </>} />
+            label={
+              <>
+                Show Hierarchical Clustering
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id="showMetabolitesTooltip">
+                      Requires at least 2 exposures and outcomes
+                    </Tooltip>
+                  }>
+                  <i className="bi bi-info-circle ms-1" />
+                </OverlayTrigger>
+              </>
+            }
+          />
         </Form.Group>
       </Form>
 
@@ -86,8 +110,8 @@ export default function HeatmapResults({ results }) {
         {...selectedHeatmapPlot}
         useResizeHandler
         className="w-100"
-        style={{ height: '800px' }}
+        style={{ height: "800px" }}
       />
     </>
-  )
+  );
 }
--- client/src/modules/analysis/results/heatmap-results.utils.js
+++ client/src/modules/analysis/results/heatmap-results.utils.js
@@ -2,30 +2,40 @@ import { groupBy, pick, cloneDeep, chunk, uniq, map } from "lodash";
 
 export function sampleChunks(values, interval) {
   const chunkSize = Math.floor(values?.length / interval) || 1;
-  return chunk(values, chunkSize).map(e => e[0])
+  return chunk(values, chunkSize).map((e) => e[0]);
 }
 
 export function getHeatmapPlot(results, heatmapOptions) {
-  if (!results || !results.heatmap || !results.heatmap.data || !results.heatmap.data.length) {
-    return {data: [], layout: {}};
+  if (
+    !results ||
+    !results.heatmap ||
+    !results.heatmap.data ||
+    !results.heatmap.data.length
+  ) {
+    return { data: [], layout: {} };
   }
 
-  const { xKey, yKey, zKey, sortColumn, sortRow } = heatmapOptions;
+  const { xKey, yKey, zKey, sortColumn } = heatmapOptions;
 
   const records = cloneDeep(results?.Effects) || [];
   const xCategories = uniq(map(records, xKey));
   const recordsGroupedByY = groupBy(records, yKey);
   const xCategoriesSorted = cloneDeep(xCategories).sort();
-  const matchesSortColumn = record => record[xKey] === (sortColumn || xCategories[0]);
-  const yCategoriesSorted = Object.entries(recordsGroupedByY).sort((a, b) => {
-    const recordA = a[1].find(matchesSortColumn);
-    const recordB = b[1].find(matchesSortColumn);
-    return recordA && recordB ? recordA[zKey] - recordB[zKey] : 0
-  }).map(([key]) => key);
+  const matchesSortColumn = (record) =>
+    record[xKey] === (sortColumn || xCategories[0]);
+  const yCategoriesSorted = Object.entries(recordsGroupedByY)
+    .sort((a, b) => {
+      const recordA = a[1].find(matchesSortColumn);
+      const recordB = b[1].find(matchesSortColumn);
+      return recordA && recordB ? recordA[zKey] - recordB[zKey] : 0;
+    })
+    .map(([key]) => key);
 
-  const values = yCategoriesSorted.map(y => xCategoriesSorted.map(x =>
-    records.find(e => e[xKey] === x && e[yKey] === y)[zKey]
-  ));
+  const values = yCategoriesSorted.map((y) =>
+    xCategoriesSorted.map(
+      (x) => records.find((e) => e[xKey] === x && e[yKey] === y)[zKey]
+    )
+  );
 
   return {
     data: [
@@ -33,37 +43,44 @@ export function getHeatmapPlot(results, heatmapOptions) {
         x: xCategoriesSorted,
         y: yCategoriesSorted,
         z: values,
-        type: 'heatmap',
+        type: "heatmap",
       },
     ],
     layout: {
-      annotations: heatmapOptions.showAnnotations ? records.map(r => ({
-        x: r[xKey],
-        y: r[yKey],
-        text: r[zKey],
-        xref: 'x',
-        yref: 'y',
-        showarrow: false,
-      })) : [],
+      annotations: heatmapOptions.showAnnotations
+        ? records.map((r) => ({
+            x: r[xKey],
+            y: r[yKey],
+            text: r[zKey],
+            xref: "x",
+            yref: "y",
+            showarrow: false,
+          }))
+        : [],
       xaxis: {
         automargin: true,
       },
       yaxis: {
         automargin: true,
       },
-    }
+    },
   };
 }
 
-
 export function getHeatmapDendrogramPlot(results, heatmapOptions) {
-  if (!results || !results.heatmap || !results.heatmap.data || !results.heatmap.dendrogram || !results.heatmap.data.length) {
-    return {data: [], layout: {}};
+  if (
+    !results ||
+    !results.heatmap ||
+    !results.heatmap.data ||
+    !results.heatmap.dendrogram ||
+    !results.heatmap.data.length
+  ) {
+    return { data: [], layout: {} };
   }
 
   const defaultInterval = 40;
   const hcluster = cloneDeep(results?.heatmap?.dendrogram);
-  const hclusterLayoutProps =  [
+  const hclusterLayoutProps = [
     "anchor",
     "automargin",
     "autorange",
@@ -81,68 +98,93 @@ export function getHeatmapDendrogramPlot(results, heatmapOptions) {
     "tickwidth",
     "type",
   ];
-  const hclusterHeatmapTrace = hcluster?.data.find(t => t.type === 'heatmap');
+  const hclusterHeatmapTrace = hcluster?.data.find((t) => t.type === "heatmap");
   let hclusterAnnotations = [];
-  
+
   if (hclusterHeatmapTrace) {
-    for (let y = 0; y < hclusterHeatmapTrace.y.length; y ++) {
-      for (let x = 0; x < hclusterHeatmapTrace.x.length; x ++) {
+    for (let y = 0; y < hclusterHeatmapTrace.y.length; y++) {
+      for (let x = 0; x < hclusterHeatmapTrace.x.length; x++) {
         hclusterAnnotations.push({
           x: hclusterHeatmapTrace.x[x],
           y: hclusterHeatmapTrace.y[y],
           text: hclusterHeatmapTrace.z[y][x],
-          xref: 'x',
-          yref: 'y2',
+          xref: "x",
+          yref: "y2",
           showarrow: false,
-        })
+        });
       }
     }
   }
 
-  return hcluster ? {
-    data: hcluster.data.map(t => {
-      const props = pick(t, ['x', 'y', 'z', 'type', 'mode', 'text', 'hoverinfo', 'xaxis', 'yaxis'])
+  return hcluster
+    ? {
+        data: hcluster.data
+          .map((t) => {
+            const props = pick(t, [
+              "x",
+              "y",
+              "z",
+              "type",
+              "mode",
+              "text",
+              "hoverinfo",
+              "xaxis",
+              "yaxis",
+            ]);
 
-      if (t.type === 'scatter' && t.x && t.x.length >= 2) {
-        return {
-          ...props,
-          showlegend: false,
-        }
+            if (t.type === "scatter" && t.x && t.x.length >= 2) {
+              return {
+                ...props,
+                showlegend: false,
+              };
+            } else if (t.type === "heatmap") {
+              return props;
+            }
+
+            return null;
+          })
+          .filter(Boolean),
+        layout: {
+          annotations: heatmapOptions.showAnnotations
+            ? hclusterAnnotations
+            : [],
+          margin: hcluster.layout.margin,
+          xaxis: {
+            ...pick(hcluster.layout.xaxis, hclusterLayoutProps),
+            tickvals: sampleChunks(
+              hcluster.layout.xaxis.tickvals,
+              defaultInterval
+            ),
+            ticktext: sampleChunks(
+              hcluster.layout.xaxis.ticktext,
+              defaultInterval
+            ),
+          },
+          xaxis2: {
+            ...pick(hcluster.layout.xaxis2, hclusterLayoutProps),
+          },
+          yaxis: {
+            ...pick(hcluster.layout.yaxis, hclusterLayoutProps),
+          },
+          yaxis2: {
+            ...pick(hcluster.layout.yaxis2, hclusterLayoutProps),
+            tickvals: sampleChunks(
+              hcluster.layout.yaxis2.tickvals,
+              defaultInterval
+            ),
+            ticktext: sampleChunks(
+              hcluster.layout.yaxis2.ticktext,
+              defaultInterval
+            ),
+          },
+        },
       }
-
-      else if (t.type === 'heatmap') {
-        return props;
-      }
-
-      return null;
-    }).filter(Boolean),
-    layout: {
-      annotations: heatmapOptions.showAnnotations ? hclusterAnnotations : [],
-      margin: hcluster.layout.margin,
-      xaxis: {
-        ...pick(hcluster.layout.xaxis, hclusterLayoutProps),
-        tickvals: sampleChunks(hcluster.layout.xaxis.tickvals, defaultInterval),
-        ticktext: sampleChunks(hcluster.layout.xaxis.ticktext, defaultInterval),
-      },
-      xaxis2: {
-        ...pick(hcluster.layout.xaxis2, hclusterLayoutProps)
-      },
-      yaxis: {
-        ...pick(hcluster.layout.yaxis, hclusterLayoutProps)
-      },
-      yaxis2: {
-        ...pick(hcluster.layout.yaxis2, hclusterLayoutProps),
-        tickvals: sampleChunks(hcluster.layout.yaxis2.tickvals, defaultInterval),
-        ticktext: sampleChunks(hcluster.layout.yaxis2.ticktext, defaultInterval),
-      },
-    }
-  } : {data: [], layout: {}};
-
+    : { data: [], layout: {} };
 }
 
 export function getHeatmapPlot2(heatmap, xSort, ySort) {
   if (!heatmap || !heatmap.data || !heatmap.data.length) {
-    return {}
+    return {};
   }
 
   // const records = heatmap.data;
@@ -157,5 +199,4 @@ export function getHeatmapPlot2(heatmap, xSort, ySort) {
   // const rows = data.map(d => d._row);
   // const columns = Objects.keys(data[0]).filter(k => k !== '_row');
   // const values = rows.map(row => columns.map(column => lookup[row][column]));
-  
 }
