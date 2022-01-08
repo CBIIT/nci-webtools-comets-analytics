9db35fa63a4afbdcd16c31d69ff9665d50cc5708
Brian Park <park-brian@users.noreply.github.com>
2022-01-07 21:34:51 -0500

    exclude count for singular nouns

--- client/src/services/text.js
+++ client/src/services/text.js
@@ -1,3 +1,3 @@
 export function pluralCount(count, singular, plural = `${singular}s`) {
-  return `${count} ${count === 1 ? singular : plural}`;
+  return count === 1 ? singular : `${count} ${plural}`;
 }
