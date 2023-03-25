0b86cb65d4dc9b06aa0075a6463fd174046ef467
Brian Park <park-brian@users.noreply.github.com>
2023-03-24 22:24:40 -0400

    simplify receiveMessage for single consumers

--- server/utils/queue.R
+++ server/utils/queue.R
@@ -1,7 +1,3 @@
-library(future)
-
-plan(multisession)
-
 receiveMessage <- function(sqs, queueName, messageHandler, errorHandler, logger, visibilityTimeout = 60) {
   queueUrl <- sqs$get_queue_url(QueueName = queueName)$QueueUrl
 
@@ -16,26 +12,10 @@ receiveMessage <- function(sqs, queueName, messageHandler, errorHandler, logger,
 
       if (length(response$Messages) > 0) {
         message <- response$Messages[[1]]
-
-        messageHandlerTask <- future({
-          shouldLog # inject shouldLog function
-          output <- callWithHandlers(messageHandler, message$Body)
-
-          if (length(output$errors) > 0) {
-            errorOutput <- callWithHandlers(errorHandler, message$Body, output)
-            logger$error(errorOutput)
-          }
-        })
-
-        while (!resolved(messageHandlerTask)) {
-          sqs$change_message_visibility(
-            QueueUrl = queueUrl,
-            ReceiptHandle = message$ReceiptHandle,
-            VisibilityTimeout = visibilityTimeout
-          )
-          Sys.sleep(1)
+        output <- callWithHandlers(messageHandler, message$Body)
+        if (length(output$errors) > 0) {
+          callWithHandlers(errorHandler, message$Body, output)
         }
-
         sqs$delete_message(
           QueueUrl = queueUrl,
           ReceiptHandle = message$ReceiptHandle
