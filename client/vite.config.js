ff85f39b44d3edb12f615412562f502162553be1
Phyllip Cho <phyllip3@users.noreply.github.com>
2025-09-30 14:06:32 -0400

    clean up vite config and dockerfile

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,45 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { execSync } from "child_process";
-import { readFileSync } from "fs";
-import process from "process";
-
-// Get version from root package.json
-const rootPackageJson = JSON.parse(readFileSync("../package.json", "utf8"));
-const version = rootPackageJson.version;
-
-// Get build date and git info
-const buildDate = new Date().toISOString().split('T')[0]; // YYYY-MM-DD format
-
-// Use environment variables if available (set during Docker build), otherwise try git commands
-let gitTag = process.env.VITE_GIT_TAG || '';
-let gitBranch = process.env.VITE_GIT_BRANCH || '';
-let lastCommitDate = process.env.VITE_LAST_COMMIT_DATE || '';
-
-// If environment variables are not set, try git commands (for local development)
-if (!gitTag) {
-  try {
-    gitTag = execSync('git describe --tags --abbrev=0', { encoding: 'utf8' }).trim();
-  } catch {
-    gitTag = 'unknown';
-  }
-}
-
-if (!gitBranch) {
-  try {
-    gitBranch = execSync('git branch --show-current', { encoding: 'utf8' }).trim();
-  } catch {
-    gitBranch = 'unknown';
-  }
-}
-
-if (!lastCommitDate) {
-  try {
-    lastCommitDate = execSync('git log -1 --format="%ci"', { encoding: 'utf8' }).trim().split(' ')[0];
-  } catch {
-    lastCommitDate = buildDate;
-  }
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -49,12 +9,5 @@ export default defineConfig({
     proxy: {
       "/api": "http://localhost:8000",
     },
-  },
-  define: {
-    __APP_VERSION__: JSON.stringify(version),
-    __BUILD_DATE__: JSON.stringify(buildDate),
-    __GIT_TAG__: JSON.stringify(gitTag),
-    __GIT_BRANCH__: JSON.stringify(gitBranch),
-    __LAST_COMMIT_DATE__: JSON.stringify(lastCommitDate),
   },
 });
--- docker/frontend.dockerfile
+++ docker/frontend.dockerfile
@@ -1,15 +1,15 @@
 FROM public.ecr.aws/amazonlinux/amazonlinux:2023
 
 RUN dnf -y update \
- && dnf -y install \
-    gcc-c++ \
-    httpd \
-    make \
-    nodejs20 \
-    nodejs20-npm  \
-    npm \
-    git \
- && dnf clean all
+   && dnf -y install \
+   gcc-c++ \
+   httpd \
+   make \
+   nodejs20 \
+   nodejs20-npm  \
+   npm \
+   git \
+   && dnf clean all
 
 RUN ln -s -f /usr/bin/node-20 /usr/bin/node; ln -s -f /usr/bin/npm-20 /usr/bin/npm;
 
@@ -17,27 +17,22 @@ RUN mkdir /client
 
 WORKDIR /client
 
-COPY package.json /package.json
 COPY client/package.json /client/
 
 RUN npm install
 
 COPY client /client/
 
-# Accept git information as build arguments
 ARG GIT_TAG=dev
 ARG GIT_BRANCH=dev  
 ARG LAST_COMMIT_DATE=dev
 
-# Set environment variables for Vite build
 ENV VITE_GIT_TAG=${GIT_TAG}
 ENV VITE_GIT_BRANCH=${GIT_BRANCH}
 ENV VITE_LAST_COMMIT_DATE=${LAST_COMMIT_DATE}
 
-# Build the application and move to web root
-RUN echo "Building with Git Info: Tag=$VITE_GIT_TAG, Branch=$VITE_GIT_BRANCH, Date=$VITE_LAST_COMMIT_DATE" && \
-    npm run build && \
-    mv /client/dist/* /var/www/html/
+RUN npm run build 
+RUN cp -r /client/dist/* /var/www/html/
 
 COPY docker/frontend.conf /etc/httpd/conf.d/frontend.conf
 
@@ -47,4 +42,4 @@ EXPOSE 80
 EXPOSE 443
 
 CMD rm -rf /run/httpd/* /tmp/httpd* \
- && exec /usr/sbin/httpd -DFOREGROUND
+   && exec /usr/sbin/httpd -DFOREGROUND
