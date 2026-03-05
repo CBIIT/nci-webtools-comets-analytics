3ccbf963d4a02e604cc4b731b833610587b35b14
nhoktenz <6723699+nhoktenz@users.noreply.github.com>
2026-03-05 17:10:34 -0500

    fix the melt error when running cohort tab

--- server/worker.R
+++ server/worker.R
@@ -1,7 +1,27 @@
 library(dotenv)
 library(jsonlite)
 library(paws)
+
+# Load reshape2 BEFORE RcometsAnalytics
+library(reshape2)
 library(RcometsAnalytics)
+
+# Patch data.table's melt.default to always use reshape2::melt for data.frames
+# This fixes the deprecation error in data.table 1.16+
+if (requireNamespace("data.table", quietly = TRUE)) {
+  # Replace data.table's melt.default with reshape2's version
+  assignInNamespace(
+    "melt.default",
+    function(data, ...) {
+      if (is.data.frame(data)) {
+        reshape2::melt(data, ...)
+      } else {
+        reshape2::melt(data, ...)
+      }
+    },
+    ns = "data.table"
+  )
+}
 
 source("utils.R")
 
